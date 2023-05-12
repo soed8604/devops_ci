95a2cbc14fb260dcbc04edec05600d4ff7e91dbd
esotelo <ejsotelo4@gmail.com>
2023-05-12 02:48:13 +0000

    prueba con slack

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicApplicationTests {
@@ -17,3 +18,4 @@ public class BasicApplicationTests {
  }
  
 }
+
