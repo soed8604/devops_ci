fbe09eb054177c3ffb91e1542672f6998271efb4
esotelo <ejsotelo4@gmail.com>
2023-05-12 02:58:29 +0000

    nueva actualización

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -45,6 +45,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc/*allow test only http incoming request layer without start the serve, 
         but starting the full spring application context*/
 public class BasicApplicationTests {
+    
   
     @Autowired
     private MockMvc mockMvc;
